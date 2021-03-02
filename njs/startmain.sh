c3aaad45dac7b4a33b911a117ffac03b6049281a
lhcdims <ken@zephan.top>
2021-03-02 12:08:11 +0800

    Restart sudo bash ./startmain.sh

---
+++ njs/startmain.sh
@@ -0,0 +1,6 @@
+sudo chmod 655 /www/server/panel
+sudo chmod 655 /www/server/panel/vhost
+sudo chmod 655 /www/server/panel/vhost/ssl
+sudo chmod 655 /www/server/panel/vhost/ssl/pg01.bigaibot.com
+sudo chmod 655 /www/server/panel/vhost/ssl/pg01.bigaibot.com/fullchain.pem
+sudo chmod 655 /www/server/panel/vhost/ssl/pg01.bigaibot.com/privkey.pem
